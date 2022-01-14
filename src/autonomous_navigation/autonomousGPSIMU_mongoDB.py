ee215ddf107046c97a9238b7bd06d957989c5841
NipuniYR <nipuniyr@gmail.com>
2022-01-14 09:40:51 +0530

    Connected the navigation script to MongoDB Database - created a new file

---
+++ src/autonomous_navigation/autonomousGPSIMU_mongoDB.py
@@ -0,0 +1,114 @@
+# To execute this script use -> python autonomousGPSIMU_mongoDB.py "<apartmentNo>"
+
+from pymongo import MongoClient
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+from pymavlink import mavutil
+import time
+import argparse 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--connect', default='127.0.0.1:14550')
+parser.add_argument('apartmentNo')
+args = parser.parse_args()
+
+######################################### Connecting to Mongo Client #########################################
+
+##CONNECTION_STRING = "mongodb://localhost:27017"
+HOST = 'localhost'
+PORT = 27017
+DATABASE = 'skydrop'
+COLLECTION = 'apartments'
+
+client = MongoClient(HOST,PORT)
+collection = client[DATABASE][COLLECTION]
+
+APARTMENT_NO = args.apartmentNo
+
+query = {"apartmentNo": APARTMENT_NO}
+
+apartmentDetails = collection.find_one(query)
+
+LONG = apartmentDetails["location"]["coordinates"][0]
+LAT = apartmentDetails["location"]["coordinates"][1]
+ALTITUDE = apartmentDetails["altitude"]
+ARUCOID = apartmentDetails["arucoID"]
+
+INITIAL_HEIGHT = 20
+
+
+######################################### Autonomous navigation #########################################
+
+# Connect to the Vehicle
+print 'Connecting to vehicle on: %s' % args.connect
+vehicle = connect(args.connect, baud=921600, wait_ready=True)
+
+
+# Function to arm and then takeoff to a user specified altitude
+def arm_and_takeoff(aTargetAltitude):
+
+  print "Basic pre-arm checks"
+  # Don't let the user try to arm until autopilot is ready
+  while not vehicle.is_armable:
+    print " Waiting for vehicle to initialise..."
+    time.sleep(1)
+        
+  print "Arming motors"
+  # Copter should arm in GUIDED mode
+  vehicle.mode    = VehicleMode("GUIDED")
+  vehicle.armed   = True
+
+  while not vehicle.armed:
+    print " Waiting for arming..."
+    time.sleep(1)
+
+  print "Taking off!"
+  vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+  # Check that vehicle has reached takeoff altitude
+  while True:
+    print " Altitude: ", vehicle.location.global_relative_frame.alt 
+    #Break and return from function just below target altitude.        
+    if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: 
+      print "Reached target altitude"
+      break
+    time.sleep(1)
+
+#Takeoff height in meters
+arm_and_takeoff(INITIAL_HEIGHT)
+
+print("Take off complete")
+
+
+print("Set default/target airspeed to 3")
+vehicle.airspeed = 3
+
+print("Going towards first point for 30 seconds ...")
+point1 = LocationGlobalRelative(LAT, LONG, INITIAL_HEIGHT)
+vehicle.simple_goto(point1)
+
+# # sleep so we can see the change in map
+time.sleep(10)
+
+print("Going towards second point for 30 seconds ...")
+point2 = LocationGlobalRelative(LAT, LONG, ALTITUDE)
+vehicle.simple_goto(point2)
+
+# # sleep so we can see the change in map
+time.sleep(30)
+
+print("Going towards third point for 30 seconds ...")
+point3 = LocationGlobalRelative(LAT, LONG, INITIAL_HEIGHT)
+vehicle.simple_goto(point3)
+
+print("Returning to Launch")
+vehicle.mode = VehicleMode("RTL")
+
+# print("Land")
+# vehicle.mode = VehicleMode("LAND")
+
+# Close vehicle object before exiting script
+print("Close vehicle object")
+vehicle.close()
+
+# Close MongoDB client
+client.close()
